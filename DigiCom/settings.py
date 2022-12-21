4821b3efb837c3f5eaa2651678c06dc78f54b89f
Raghav Rastogi <Raghav.Rastogi.Contractor@pepsico.com>
2022-12-21 19:25:18 +0530

    added csrf trusted origins for 403 forbidden csrf error

--- DigiCom/settings.py
+++ DigiCom/settings.py
@@ -243,3 +243,8 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "*"
+]
